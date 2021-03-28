34677a9f5adef0211bb0353baecd6a2438e2664f
JIGAR-PATEL <jigarpatel7600@gmail.com>
2021-03-28 08:56:40 +0530

    beta

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,14 +2,14 @@ import 'package:flutter/material.dart';
 
 //TODO: Edit these values;
 //BLogspot url (please include like in reference)
-const String kBlogUrl = 'https://www.arlinadzgn.com/';
+const String kBlogUrl = 'https://www.yourblogspoturl.com/';
 //Google Console api key
-const String kApiKey = 'AIzaSyBxKWHerHdUqpu4boo4pmAfxla5vH1X5bg';
+const String kApiKey = 'AIzaXXXXXXXXXXXXXXXXXXXXXXXXXX';
 //Material Color Theme
 const Color kAppColor = Colors.red;
 //App title
-const String kAppTitle = 'Blogger';
+const String kAppTitle = 'Blog App';
 //Admob app id
-const String kAdmobAppId = 'ca-app-pub-3940256099942544~3347511713';
+const String kAdmobAppId = 'ca-app-pub-394XXXXXXXXXXXX~XXXXXXXXXX';
 //Admob banner id
-const String kAdmobBannerId = 'ca-app-pub-3940256099942544/6300978111';
+const String kAdmobBannerId = 'ca-app-pub-3940XXXXXXXXXX/XXXXXXXXXX';
